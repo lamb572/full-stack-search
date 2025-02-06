0d82da2529fb3456bf6dff623312bccdcf68b890
lamb572 <lukebeach90@gmail.com>
2025-02-06 12:56:50 +0000

    Implement text search index's

--- packages/api/db/startAndSeedMemoryDB.ts
+++ packages/api/db/startAndSeedMemoryDB.ts
@@ -1,34 +1,53 @@
-import { MongoMemoryServer } from "mongodb-memory-server";
-import { MongoClient } from "mongodb";
-import { cities } from "db/seeds/cities.js";
-import { countries } from "./seeds/countries";
-import { hotels } from "./seeds/hotels";
+import { MongoMemoryServer } from "mongodb-memory-server"
+import { MongoClient } from "mongodb"
+import { cities } from "db/seeds/cities.js"
+import { countries } from "./seeds/countries"
+import { hotels } from "./seeds/hotels"
 
 const mongod = await MongoMemoryServer.create({
   instance: {
     port: 3002,
-  }
-});
-console.log("MongoMemoryServer started on", mongod.getUri());
+  },
+})
+console.log("MongoMemoryServer started on", mongod.getUri())
 
-const uri = mongod.getUri();
+const uri = mongod.getUri()
 
-process.env.DATABASE_URL = uri;
+process.env.DATABASE_URL = uri
 
-const client = new MongoClient(uri);
+const client = new MongoClient(uri)
 try {
-  await client.connect();
-  const db = client.db();
-  await db.collection("cities").insertMany(cities);
-  await db.collection("countries").insertMany(countries);
-  await db.collection("hotels").insertMany(hotels);
+  await client.connect()
+  const db = client.db()
+  await db.collection("cities").insertMany(cities)
+  await db.collection("cities").createIndex({
+    name: "text",
+  })
+  await db.collection("countries").insertMany(countries)
+  await db.collection("countries").createIndex({
+    country: "text",
+    countryisocode: "text",
+  })
+
+  await db.collection("hotels").insertMany(hotels)
+  await db.collection("hotels").createIndex({
+    chain_name: "text",
+    hotel_name: "text",
+    addressline1: "text",
+    addressline2: "text",
+    zipcode: "text",
+    city: "text",
+    state: "text",
+    country: "text",
+    countryisocode: "text",
+  })
 } catch (error) {
-  console.error("Error seeding database:", error);
+  console.error("Error seeding database:", error)
 } finally {
-  await client.close();
+  await client.close()
 }
 
-process.on('SIGTERM', async () => {
-  await mongod.stop();
-  process.exit(0);
-});
+process.on("SIGTERM", async () => {
+  await mongod.stop()
+  process.exit(0)
+})
